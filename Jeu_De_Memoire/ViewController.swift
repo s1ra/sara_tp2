1c6a0611b8e8246bf6d2c90b29057c0b552b4c2d
sarah <sarah@macbook-pro-de-sido.local>
2017-09-23 11:30:21 -0400

    Initial Commit

---
+++ Jeu_De_Memoire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jeu_De_Memoire
+//
+//  Created by sarah on 17-09-23.
+//  Copyright © 2017 sara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
